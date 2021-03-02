6ca2bdd019cb9c4b76ec53f375564a1a1d0041d2
Dom1L <lemm.d@web.de>
2021-03-02 17:43:12 +0100

    read wbo instead of topo from mol file

--- g2s/graph_extractor/xtb.py
+++ g2s/graph_extractor/xtb.py
@@ -2,11 +2,10 @@ import os
 import subprocess
 import tempfile
 
-try:
-    from rdkit import Chem
-    from .smiles import get_bo, rdkit_chirality
-except ModuleNotFoundError:
-    raise ModuleNotFoundError('Install RDKit if you want to use this feature!')
+import numpy as np
+
+from ..utils import read_xyz
+from ..constants import periodic_table
 
 
 def run_xtb(cmd):
@@ -28,15 +27,28 @@ def xtb_opt(filepath, outpath=None, opt=True, ncores=1):
     return outpath
 
 
-def xtb_graph(filepath, outpath=None, ncores=1, opt=False, get_chirality=False):
+def xtb_graph(filepath, outpath=None, ncores=1, opt=False):
     xtb_out = xtb_opt(filepath, outpath=outpath, opt=opt, ncores=ncores)
 
-    mol = Chem.SDMolSupplier(f'{xtb_out}/xtbtopo.mol')[0]
-    coords = mol.GetConformer().GetPositions()
-    bond_order_matrix, nuclear_charges = get_bo(mol)
+    elements, coords = read_xyz(filepath)
+    nuclear_charges = np.array([periodic_table.index(e) for e in elements])
+    # mol = Chem.MolFromMolFile(f'{xtb_out}/xtbtopo.mol', sanitize=False, removeHs=False, strictParsing=False)
+    # coords = mol.GetConformer().GetPositions()
+    bond_order_matrix = get_bo(f'{xtb_out}/wbo', natoms=len(elements))
 
-    if get_chirality is False:
-        return bond_order_matrix, nuclear_charges, coords
-    else:
-        chiral_centers = rdkit_chirality(mol)
-        return bond_order_matrix, nuclear_charges, coords, chiral_centers
+    return bond_order_matrix, nuclear_charges, coords
+
+
+def get_bo(wbo_file, natoms):
+    with open(wbo_file, 'r') as infile:
+        lines = infile.readlines()
+
+    bo_mat = np.zeros((natoms, natoms))
+    for line in lines:
+        i, j, wbo = line.split()
+        i = int(i) - 1
+        j = int(j) - 1
+
+        bo_mat[i, j] = round(float(wbo))
+        bo_mat[j, i] = round(float(wbo))
+    return bo_mat.astype(int)
