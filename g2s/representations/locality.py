106edead3a8672c381e425a9220252fcb23d7379
Dom1L <lemm.d@web.de>
2021-03-11 20:51:02 +0100

    knn locality testing

--- g2s/representations/locality.py
+++ g2s/representations/locality.py
@@ -1,4 +1,5 @@
 import numpy as np
+import igraph
 
 
 def get_local_environment(index, representation, nuclear_charges,
@@ -25,15 +26,109 @@ def get_local_environment(index, representation, nuclear_charges,
         return atomic_representation[row, col], local_nuclear_charges, local_idxs
 
 
+def get_full_local_knn_environment(idx, adjacency_matrix, representation, nuclear_charges, distances=None, depth=2, sorting='norm_row'):
+    graph = igraph.Graph()
+    graph = graph.Adjacency(list(adjacency_matrix.astype(float)))
+    # Get all K=1 neighbors around a query atom
+    neighbor_idxs = graph.neighbors(idx, mode='OUT')
+    new_neighs = neighbor_idxs
+    d = 1
+    # if k > 1 then scan further neighbors
+    while d < depth:
+        kn = []
+        for n in new_neighs:
+            kn.append(graph.neighbors(n, mode='OUT'))
+
+        # Filter non-unique indices
+        kn = [item for sublist in kn for item in sublist]
+        kn = list(filter((idx).__ne__, kn))
+        kn = np.unique(kn)
+
+        neighbor_idxs.extend(kn)
+        new_neighs = kn
+        d += 1
+    neighbor_idxs = np.unique(neighbor_idxs)
+
+    local_idxs = np.array([idx, *neighbor_idxs])
+    row, col = np.triu_indices(len(local_idxs), k=1)
+
+    atomic_representation = representation[local_idxs][:, local_idxs]
+    local_nuclear_charges = nuclear_charges[local_idxs]
+    if distances is not None:
+        local_distances = distances[local_idxs][:, local_idxs]
+
+    if sorting == 'norm_row':
+        row_norm_idx = np.argsort(np.linalg.norm(atomic_representation, axis=1))
+        atomic_representation = atomic_representation[row_norm_idx][:, row_norm_idx]
+        local_nuclear_charges = local_nuclear_charges[row_norm_idx]
+        local_idxs = local_idxs[row_norm_idx]
+
+        if distances is not None:
+            local_distances = local_distances[row_norm_idx][:, row_norm_idx]
+    if distances is not None:
+        return atomic_representation[row, col], local_nuclear_charges, local_idxs, local_distances[row, col]
+    else:
+        return atomic_representation[row, col], local_nuclear_charges, local_idxs
+
+
+def get_local_knn_environment(idx, adjacency_matrix, representation, nuclear_charges, distances=None, depth=2, sorting='norm_row'):
+    graph = igraph.Graph()
+    graph = graph.Adjacency(list(adjacency_matrix.astype(float)))
+    # Get all K=1 neighbors around a query atom
+    neighbor_idxs = graph.neighbors(idx, mode='OUT')
+    new_neighs = neighbor_idxs
+    d = 1
+    # if k > 1 then scan further neighbors
+    while d < depth:
+        kn = []
+        for n in new_neighs:
+            kn.append(graph.neighbors(n, mode='OUT'))
+
+        # Filter non-unique indices
+        kn = [item for sublist in kn for item in sublist]
+        kn = list(filter((idx).__ne__, kn))
+        kn = np.unique(kn)
+
+        neighbor_idxs.extend(kn)
+        new_neighs = kn
+        d += 1
+    neighbor_idxs = np.unique(neighbor_idxs)
+
+    # row, col = np.triu_indices(len(local_idxs), k=1)
+
+    atomic_representation = representation[idx][neighbor_idxs]
+    local_nuclear_charges = nuclear_charges[neighbor_idxs]
+    if distances is not None:
+        local_distances = distances[idx][neighbor_idxs]
+
+    if sorting == 'norm_row':
+        row_norm_idx = np.argsort(np.linalg.norm(atomic_representation, axis=1))
+        atomic_representation = atomic_representation[row_norm_idx]
+        local_nuclear_charges = local_nuclear_charges[row_norm_idx]
+        neighbor_idxs = neighbor_idxs[row_norm_idx]
+
+        if distances is not None:
+            local_distances = local_distances[row_norm_idx]
+
+    local_nuclear_charges = np.array([nuclear_charges[idx], *local_nuclear_charges])
+    local_idxs = np.array([idx, *neighbor_idxs])
+
+    if distances is not None:
+        return atomic_representation, local_nuclear_charges, local_idxs, local_distances
+    else:
+        return atomic_representation, local_nuclear_charges, local_idxs
+
+
 def get_unique_environments(local_environments, local_idxs, local_distances=None):
     uq_env = {}
-    for i, r in enumerate(local_environments):
-        if tuple(r) not in uq_env.keys():
-            uq_env[tuple(r)] = {'local_idxs': [],
-                                'local_dist': []}
-        uq_env[tuple(r)]['local_idxs'].append(local_idxs[i])
-        if local_distances is not None:
-            uq_env[tuple(r)]['local_dist'].append(local_distances[i])
+    for j in range(len(local_environments)):
+        for i, r in enumerate(local_environments[j]):
+            if tuple(r) not in uq_env.keys():
+                uq_env[tuple(r)] = {'local_idxs': [],
+                                    'local_dist': []}
+            uq_env[tuple(r)]['local_idxs'].append(local_idxs[j][i])
+            if local_distances is not None:
+                uq_env[tuple(r)]['local_dist'].append(local_distances[j][i])
 
     if local_distances is not None:
 
@@ -42,13 +137,13 @@ def get_unique_environments(local_environments, local_idxs, local_distances=None
     return uq_env
 
 
-def construct_sparse_dm(local_distances, local_idxs, n_atoms, n_neighs=4):
-    row, col = np.triu_indices(n_neighs, k=1)
+def construct_sparse_dm(local_distances, local_idxs, n_atoms):
     _r, _c = np.triu_indices(n_atoms, k=1)
 
     sparse_dm = np.zeros((n_atoms, n_atoms))
     d = {(r, c): [] for r, c in zip(_r, _c)}
     for ld, li in zip(local_distances, local_idxs):
+        row, col = np.triu_indices(local_idxs, k=1)
         for i in range(len(row)):
             atom_i = li[row[i]]
             atom_j = li[col[i]]
