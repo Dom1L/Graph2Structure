86afdf7e1e29a0ff7f92f5508e396c90c68a30c7
Dom1L <lemm.d@web.de>
2021-03-23 20:24:19 +0100

    test g2s molassembler code

--- tests/test_molassembler.py
+++ tests/test_molassembler.py
@@ -6,7 +6,9 @@ import numpy as np
 from g2s.constants import periodic_table
 from g2s.graph_extractor import xyz2mol_graph
 from g2s.dgeom.molassembler import graph_to_molass, bo_from_molassembler
+from g2s.utils import calculate_distances, write_xyz
 
+import scine_molassembler as scm
 
 def test_conversion(filepath):
     adj, nuclear_charges, coords = xyz2mol_graph(filepath)
@@ -19,12 +21,13 @@ def test_conversion(filepath):
     assert np.sum(m_bo - adj[reverse_map, :][:, reverse_map]) == 0.
 
 
-
-def test_confgen(filepath):
-    adj, nuclear_charges, coords = xyz2mol_graph(filepath)
+def test_distance_bounds():
+    adj, nuclear_charges, coords = xyz2mol_graph(test_mols[0])
     elements = [periodic_table[nc] for nc in nuclear_charges]
     graph, reverse_map, bkp_adj = graph_to_molass(adj, elements, debug=True)
+    distances = calculate_distances(coords)
 
+    new_coords = scm.dg.generate_g2s_conformation(graph, distances[reverse_map, :][:, reverse_map], 13)*0.529177
 
 if __name__ == '__main__':
     test_mols = sorted(glob('/data/lemm/g2s/const_isomers/*.xyz'))
