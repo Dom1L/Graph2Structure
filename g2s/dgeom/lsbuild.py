773d62c66ff685b57b4ec3fb2a8a7040946f715a
Dom1L <lemm.d@web.de>
2021-04-21 07:36:45 +0200

    numba lsbuild

--- g2s/dgeom/lsbuild.py
+++ g2s/dgeom/lsbuild.py
@@ -1,101 +1,423 @@
 import numpy as np
-from scipy.spatial.distance import squareform
-from g2s.utils import calculate_distances, read_xyz
+import networkx as nx
+from numpy import sqrt, zeros, sum
+from numpy.linalg import svd, norm, lstsq
 from scipy.optimize import minimize
-
 from numba import jit
 
-
-def lsbuild(boundary_matrix, _lambda=1.0, _tau=1e-5):
-    n_atoms = boundary_matrix.shape[0]
-    coords = np.zeros((n_atoms, 3))
-    distance_matrix = init_dmat(boundary_matrix=boundary_matrix, seed=42)
-    base_ids = np.arange(4)
-    base_coords = embed_base(distance_matrix[base_ids][:, base_ids])
-    n_base_atoms = base_coords.shape[0]
-    base_row, base_col = np.triu_indices(n_base_atoms, k=1)
-    res = minimize(opt_func, base_coords, method='BFGS',
-                   args=(distance_matrix[(base_col, base_row)],
-                         distance_matrix[(base_row, base_col)],
-                         _lambda, _tau,
-                         n_base_atoms, base_row, base_col, _lambda * (distance_matrix[(base_col, base_row)] - distance_matrix[(base_row, base_col)])))
-    coords[base_ids, :] = res.x.reshape(n_base_atoms, 3)
-
-    remaining = np.arange(n_base_atoms, n_atoms)
-    _base_ids = [*base_ids]
-    for i in remaining:
-        new_coords = get_undetermined(coords[base_ids, :], distance_matrix, i, base_ids)
-        _base_ids.append(i)
-
-        n_base_atoms = len(_base_ids)
-        base_ids = np.array(_base_ids)
-
-        coords[i, :] = new_coords
-        base_d = distance_matrix[base_ids][:, base_ids]
-        base_row, base_col = np.triu_indices(n_base_atoms, k=1)
-        res = minimize(opt_func, coords[base_ids, :], method='BFGS',
-                       args=(base_d[(base_col, base_row)],
-                             base_d[(base_row, base_col)],
-                             _lambda, _tau,
-                             n_base_atoms, base_row, base_col, _lambda * (base_d[(base_col, base_row)] - base_d[(base_row, base_col)])))
-        coords[base_ids, :] = res.x.reshape(n_base_atoms, 3)
-    return coords
+from g2s.constants import vdw_radii
 
 
-def init_dmat(boundary_matrix, seed=42):
-    n_atoms = boundary_matrix.shape[0]
-    row, col = np.triu_indices(n_atoms, k=1)
-    np.random.seed(seed)
-    t = np.random.uniform(0, 1, size=(len(row)))
-    return squareform(t * boundary_matrix[col, row] + (1 - t) * boundary_matrix[(row, col)])
+class edge_t:
+    def __init__(self, i, j, l, u):
+        self.i = i
+        self.j = j
+        self.l = l
+        self.u = u
 
 
-def embed_base(distance_matrix):
-    u, s, vh = np.linalg.svd(distance_matrix)
-    eigen_val_idx = np.argsort(-s)
-    e_tilde = np.zeros((3, 3))
-    e_tilde[np.eye(3).astype(bool)] = s[eigen_val_idx[:3]]
-    u_tilde = u[:, eigen_val_idx[:3]]
-    return np.dot(u_tilde, np.sqrt(e_tilde))
+class get_graph:
+    def __init__(self, dmat, nuclear_charges):
+        self.edgeEqu = []  # dij == lij == uij (exact distance information)
+        self.edgeBnd = []  # lij <= dij <= uij (bound distance information)
+
+        n, m = np.triu_indices(dmat.shape[1], k=1)
+        for i, j in zip(n, m):
+            u = dmat[i, j]
+            l = dmat[j, i]
+            if dmat[i, j] == 0.0:
+                u = 20.
+                l = 1. if nuclear_charges is None else vdw_radii[nuclear_charges[i]] + vdw_radii[
+                    nuclear_charges[j]]
+            edge = edge_t(i, j, l, u)
+            if l == u:
+                self.edgeEqu.append(edge)
+            else:
+                self.edgeBnd.append(edge)
+        self.G = self.init_graph()
+        self.numNodes = len(self.G)
+        self.numEdgeEqu = len(self.edgeEqu)
+        self.numEdgeBnd = len(self.edgeBnd)
+
+    def init_graph(self):
+        G = nx.Graph()
+        for edge in self.edgeEqu + self.edgeBnd:
+            G.add_edge(edge.i, edge.j, l=edge.l, u=edge.u)
+        return G
 
 
-@jit(nopython=True, fastmath=True)
-def opt_func(x, dist_l, dist_u, _lambda, _tau, n_atoms, row, col, cofactor):
-    x = x.reshape(n_atoms, 3)
-    _coord_dist = np.expand_dims(x, 0) - np.expand_dims(x, 1)
-    # coord_dist = np.sum(_coord_dist[row, col] ** 2, axis=1)
-
-    coord_dist = []
-    for r, c in zip(row, col):
-        coord_dist.append(np.sum(_coord_dist[r, c] ** 2))
-    coord_dist = np.array(coord_dist)
-    b = np.sqrt((_lambda ** 2) * (dist_l - np.sqrt(coord_dist + (_tau ** 2))) ** 2 + _tau ** 2)
-    c = np.sqrt((_lambda ** 2) * (dist_u - np.sqrt(coord_dist + (_tau ** 2))) ** 2 + _tau ** 2)
-    return np.sum(cofactor+b+c)
-
-
-# # @jit(nopython=True)
-# def omega_eq(x, c, _lambda, _tau):
-#     return
-
-
-def get_undetermined(base_coords, distance_matrix, unknown_idx, base_ids):
-    a = -2 * np.array([base_coords[base_ids[i + 1]] -
-                       base_coords[base_ids[i]] for i in range(0, len(base_coords) - 1)])
-
-    b = np.array([((distance_matrix[base_ids[i + 1], unknown_idx] ** 2 -
-                    distance_matrix[i, unknown_idx] ** 2) -
-                   (np.linalg.norm(base_coords[i + 1]) -
-                    np.linalg.norm(base_coords[i])))
-                  for i in range(0, len(base_coords) - 1)])
-
-    return np.linalg.lstsq(a, b, rcond=None)[0]
+def calcNodeMaxRelRes(G, lstB, setB, x, i):
+    maxRelRes = 0.0
+    if i in lstB:
+        xi = x[i]
+        for j in G.neighbors(i):
+            if not setB[j]:  # only nodes whose coords are fixed
+                continue
+            xj = x[j]
+            dij = norm(xi - xj)
+            gij = G[i][j]
+            lij, uij = gij['l'], gij['u']
+            mij = (lij + uij)
+            relResL, relResU = (lij - dij) / mij, (dij - uij) / mij
+            # ijMaxRelErr = max(relErrL, relErrU)
+            relRes = relResU if relResU > relResL else relResL
+            if relRes > 0 and maxRelRes < relRes:  # maxRelErr can be negative
+                maxRelRes = relRes
+    else:
+        for j in G.neighbors(i):
+            if not setB[j]:
+                continue
+            relRes = calcNodeMaxRelRes(G, lstB, setB, x, j)
+            if relRes > maxRelRes:
+                maxRelRes = relRes
+    return maxRelRes
 
 
-if __name__ == '__main__':
-    test_mol = '/Users/c0uch1/Downloads/octane.xyz'
-    test_nc, test_coords = read_xyz(test_mol)
-    test_distances = calculate_distances(test_coords)
+def calcBaseMaxRelRes(G, lstB, setB, x):
+    n = len(lstB)
+    maxRelRes = 0.0
+    for k in range(n):
+        i = lstB[k]
+        xi = x[i]
+        for j in G.neighbors(i):
+            if j > i or not setB[j]:
+                continue
+            xj = x[j]
+            dij = norm(xi - xj)
+            gij = G[i][j]
+            lij, uij = gij['l'], gij['u']
+            mij = (lij + uij)
+            relResL, relResU = (lij - dij) / mij, (dij - uij) / mij
+            # ijMaxRelErr = max(relErrL, relErrU)
+            relRes = relResU if relResU > relResL else relResL
+            if relRes > 0 and maxRelRes < relRes:  # maxRelErr can be negative
+                maxRelRes = relRes
+    return maxRelRes
 
-    __coords = lsbuild(test_distances)
-    print(np.mean(np.abs(calculate_distances(__coords) - test_distances)))
+
+@jit(nopython=True, fastmath=True, cache=True)
+def smoothMaxZero(z, tau, lam):
+    lam_z = lam * z
+    fz = lam_z + sqrt(lam_z ** 2 + tau)
+    gz = lam * fz / (fz - lam_z)
+    return fz, gz
+
+
+@jit(nopython=True, fastmath=True, cache=True)
+def smoothDistance(yi, yj, tau):
+    fy = sqrt(sum(yi ** 2) + sum(yj ** 2) - 2 * (yi @ yj) + tau)
+    gz = (yi - yj) / fy
+    return fy, gz
+
+
+@jit(nopython=True, fastmath=True, cache=True)
+def funNLP(y, tau, lam, L, U):
+    f = 0.0
+    g = np.zeros(len(y))
+    for l, u in zip(L, U):
+        # for i in L:
+        i, j, l_val = l
+        u_val = u[2]
+
+        yi = y[(3 * i):(3 * (i + 1))]
+        # for j in L[i]:
+        yj = y[(3 * j):(3 * (j + 1))]
+        fdij, gdij = smoothDistance(yi, yj, tau)
+        zlij = l_val - fdij
+        zuij = fdij - u_val
+        fmlij, gmlij = smoothMaxZero(zlij, tau, lam)
+        fmuij, gmuij = smoothMaxZero(zuij, tau, lam)
+        aij = gmuij - gmlij
+        f += fmlij + fmuij
+        g[(3 * i):(3 * (i + 1))] += aij * gdij
+        g[(3 * j):(3 * (j + 1))] -= aij * gdij
+    return (f, g)
+
+
+def checkDiff(f, y, args, rad=1, ntests=50, tol=1e-3):
+    # check derivatives of f around y using numerical approximation as probe.
+    ynrm = norm(y)
+    dy = ynrm / 1000
+    maxRelErr = 0
+    for _ in range(ntests):
+        x = y + np.random.uniform(-1, 1, size=y.shape) * ynrm
+        _, g = f(x, args)
+        gnum = np.zeros(y.shape, dtype=float)
+        xfw = x.copy()
+        xbk = x.copy()
+        for i in range(len(y)):
+            xfw[i] = x[i] + dy
+            xbk[i] = x[i] - dy
+            ffw, _ = f(xfw, args)
+            fbk, _ = f(xbk, args)
+            gnum[i] = (ffw - fbk) / (2 * dy)
+            # reset xfw and xbw
+            xfw[i] = x[i]
+            xbk[i] = x[i]
+        relErr = norm(g - gnum) / norm(g)
+        if relErr > maxRelErr:
+            maxRelErr = relErr
+        if relErr > tol:
+            raise Exception('Numerical diff and g are different.')
+
+
+def saveResult(nmr, summary, ans):
+    fn_ans = nmr.file.replace('.nmr', '.ans')
+    print('Writing ' + fn_ans)
+    with open(fn_ans, 'w') as fid:
+        fid.write(summary + '\n')
+        fid.write('\nCoords [ x y z ] =====\n')
+        x = ans['x']
+        for i in range(nmr.numNodes):
+            fid.write('% 12.8g % 12.8g % 12.8g\n' %
+                      (x[i, 0], x[i, 1], x[i, 2]))
+
+
+def lsbuild(distance_matrix, nuclear_charges, lstB=None, setB=None, t=0.5):
+    nmr = get_graph(distance_matrix, nuclear_charges)
+    tolMaxRelRes = 1e-4  # tolerance of distance constraints
+    G = nmr.G
+    if lstB is None and setB is None:
+        lstB, setB = initB(G)
+    x = initX(G, lstB, t=t)
+    maxRelRes = calcBaseMaxRelRes(G, lstB, setB, x)
+    print('Base (maxRelRes: %.3e)' % maxRelRes)
+    if maxRelRes > tolMaxRelRes:  # refining solution?
+        solveNLP(G, setB, lstB, x, False, None)
+        maxRelRes = calcBaseMaxRelRes(G, lstB, setB, x)
+        print('   NLP   (maxRelRes: %.3e)' % maxRelRes)
+    # rank of unsolved (not fixed) atoms
+    nodeRnk, nodeIdx = initNodeRnk(G, lstB, setB, x)
+    for k in range(len(lstB), nmr.numNodes):
+        i = nodeRnk[k]['node']
+        if nodeRnk[k]['deg'] < 4:
+            # There is not enough info to set i-th node
+            print('Warning: Not all nodes were solved.')
+            break
+        # add i to the solved set of vertices
+        setB[i] = True
+        lstB.append(i)
+        nodeRnk[k]['deg'] = np.inf
+        # calc coords
+        solveLSTSQ(G, setB, x, i, t=t)
+        maxRelRes = calcNodeMaxRelRes(G, lstB, setB, x, i)
+        if maxRelRes > tolMaxRelRes:
+            solveNLP(G, setB, lstB, x, False, None)
+            maxRelRes = calcBaseMaxRelRes(G, lstB, setB, x)
+        # TODO This step is O(n * max(deg)) and it could be approximately done
+        updtNodeRnk(G, lstB, setB, x, i, nodeRnk, nodeIdx)
+    notSolvedNodes = [i for i in range(nmr.numNodes) if not setB[i]]
+    for k in notSolvedNodes:
+        i = nodeRnk[k]['node']
+        if nodeRnk[k]['deg'] < 4:
+            # There is not enough info to set i-th node
+            print('Warning: Not all nodes were solved.')
+            break
+        # add i to the solved set of vertices
+        setB[i] = True
+        lstB.append(i)
+        nodeRnk[k]['deg'] = np.inf
+        # calc coords
+        solveLSTSQ(G, setB, x, i, t=t)
+        maxRelRes = calcNodeMaxRelRes(G, lstB, setB, x, i)
+        if maxRelRes > tolMaxRelRes:
+            solveNLP(G, setB, lstB, x, False, None)
+            maxRelRes = calcBaseMaxRelRes(G, lstB, setB, x)
+        # TODO This step is O(n * max(deg)) and it could be approximately done
+        updtNodeRnk(G, lstB, setB, x, i, nodeRnk, nodeIdx)
+    ans = {
+        'x': x,
+        'maxRelRes': maxRelRes,
+        'numSolvedNodes': len(lstB),
+        'notSolvedNodes': notSolvedNodes,
+    }
+    return ans
+
+
+def updtNodeRnk(G, lstB, setB, x, i, nodeRnk, nodeIdx):
+    # update maxRelRes of nodes on B
+    for j in lstB:
+        jidx = nodeIdx[j]
+        nodeRnk[jidx]['maxRelRes'] = calcNodeMaxRelRes(G, lstB, setB, x, j)
+
+    # update naxRelRes of nodes out of B
+    V = np.zeros(len(nodeRnk), dtype=bool)  # set of updated vertices
+    V[lstB] = True
+    for k in lstB:
+        for j in G.neighbors(k):
+            if V[j]:
+                continue
+            V[j] = True
+            jidx = nodeIdx[j]
+            nodeRnk[jidx]['maxRelRes'] = calcNodeMaxRelRes(G, lstB, setB, x, j)
+
+    # update degree of nodes out of B
+    for j in G.neighbors(i):
+        if j in setB:
+            continue
+        jidx = nodeIdx[j]
+        nodeRnk[jidx]['deg'] += 1
+
+    # sort nodeRnk
+    sortNodeRnk(nodeRnk)
+    for k, u in enumerate(nodeRnk):
+        nodeIdx[u['node']] = k
+
+
+def initX(G, lstB, t=0.5):
+    # x[lstB[0]] is set to [0,0,0] (anchor)
+    x = zeros((len(G.nodes), 3), dtype=float)
+    M = zeros((3, 3), dtype=float)
+    # TODO: It could be better to select a node with the best data (tight boundaries)
+    # create the approximated squadred distance matrix
+    D = zeros((4, 4), dtype=float)
+    n = len(lstB)
+    for iIdx in range(n):
+        i = lstB[iIdx]
+        for jIdx in range(iIdx + 1, n):
+            j = lstB[jIdx]
+            lij, uij = G[i][j]['l'], G[i][j]['u']
+            dij = ((1 - t) * lij + t * uij) ** 2
+            D[iIdx, jIdx], D[jIdx, iIdx] = dij, dij
+    # set M
+    for i in range(3):
+        for j in range(i, 3):
+            mij = (D[0, i + 1] + D[0, j + 1] - D[i + 1, j + 1]) / 2.0
+            M[i, j], M[j, i] = mij, mij
+    U, S, _ = svd(M)
+    # scaling cols by the largest singular values
+    for i in range(3):
+        U[:, i] *= sqrt(S[i])
+    # set values
+    for i in range(3):
+        x[lstB[i + 1]] = U[i]
+    return x
+
+
+def sortNodeRnk(nodeRnk):
+    # TODO Some specialized algorithm could be used here
+    nodeRnk.sort(key=lambda u: (-u['deg'], u['maxRelRes']))
+
+
+def initNodeRnk(G, lstB, setB, x):
+    # TODO Consider to store maxRelRes of each node on setB to reduce calcs
+    nodeRnk = []
+    for i in G:  # this loop is O(nnodes * max(deg(G)))
+        # ensure that fixed nodes (on B) will be the first ones after sorting
+        iNeighsB = [j for j in G.neighbors(i) if setB[j]]
+        deg = np.inf if setB[i] else len(iNeighsB)
+        maxRelRes = calcNodeMaxRelRes(G, lstB, setB, x, i)
+        nodeRnk.append({'node': i, 'deg': deg, 'maxRelRes': maxRelRes, })
+    sortNodeRnk(nodeRnk)
+    nodeIdx = np.zeros(len(nodeRnk), dtype=int)
+    for k in range(len(nodeRnk)):
+        nodeIdx[nodeRnk[k]['node']] = k
+    return nodeRnk, nodeIdx
+
+
+def initB(G):
+    # TODO We may consider all cliques and get the "best" of them (resp. to some stability criterion) or just a good enough clique for the sake of time efficiency.
+    # Reference:
+    # Östergård, Patric RJ. "A fast algorithm for the maximum clique problem." Discrete Applied Mathematics 120.1-3 (2002): 197-207.
+
+    # look for a base that allows to fix the maximum number of nodes
+    numBases = 0  # number of tested bases
+    numNodes = len(G.nodes)
+    maxB = []
+    maxLenB = 0  # largest base len
+    for v1 in G.nodes:
+        N1 = set(G.neighbors(v1))
+        if len(N1) < 3:
+            continue
+        for v2 in N1:
+            if v2 < v1:  # removing symmetry
+                continue
+            N2 = set(G.neighbors(v2)) & N1
+            if len(N2) < 2:
+                continue
+            for v3 in N2:
+                if v3 < v1 or v3 < v2:  # removing symmetry
+                    continue
+                N3 = set(G.neighbors(v3)) & N2
+                if len(N1) < 1:
+                    continue
+                for v4 in N3:
+                    if v4 < v1 or v4 < v2 or v4 < v3:  # removing symmetry
+                        continue
+                    numBases += 1
+                    # numFixNeighs[i]: num of neighs of vertice i already fixed
+                    numFixNeighs = zeros(numNodes, dtype=int)
+                    # the base is fixed by construction
+                    numFixNeighs[v1], numFixNeighs[v2], numFixNeighs[v3], numFixNeighs[v4] = 4, 4, 4, 4
+                    F = {v1, v2, v3, v4}  # nodes to be fixed
+                    B = set()  # set of fixed nodes
+                    lenB = len(B)
+                    while len(F) > 0:
+                        v = F.pop()
+                        B.add(v)
+                        lenB += 1
+                        for u in G.neighbors(v):
+                            # ensure that u will be added once
+                            # u has one more fixed neigh (which is v)
+                            numFixNeighs[u] += 1
+                            if numFixNeighs[u] == 4:
+                                F.add(u)
+                    if lenB > maxLenB:
+                        maxLenB = lenB
+                        maxB = B.copy()
+                        lstB = [v1, v2, v3, v4]
+                        print('Updating Bmax (Blen = %d)' % lenB)
+    print('Optimal base %s found after %d tests.' % (str(lstB), numBases))
+    if len(maxB) < numNodes:
+        F = set(G.nodes) - maxB
+        print('Warning:')
+        print('   Only %d/%d nodes can be solved (fixed).' %
+                     (maxLenB, numNodes))
+        print(
+            '   Nodes %s will not fixed (not enough constraints).' % (str([i + 1 for i in F])))
+    setB = np.zeros(numNodes, dtype=bool)
+    setB[lstB] = True
+    return lstB, setB
+
+
+def solveLSTSQ(G, setB, x, i, t=0.5):
+    iNeighB = [j for j in G.neighbors(i) if setB[j]]
+    n = len(iNeighB)
+    A = np.zeros((n - 1, 3), dtype=float)
+    b = np.zeros(n - 1, dtype=float)
+    for k, j in enumerate(iNeighB):
+        xj = x[j]
+        gij = G[i][j]
+        lij, uij = gij['l'], gij['u']
+        dij = (1.0 - t) * lij + t * uij
+        aij = dij ** 2 - norm(xj) ** 2
+        if k == 0:
+            x0, ai0 = x[j], aij
+        else:
+            A[k - 1, :] = x0 - xj
+            b[k - 1] = (aij - ai0) / 2.0
+    x[i] = lstsq(A, b, rcond=None)[0]
+
+
+def solveNLP(G, setB, lstB, x, check, tm, tau=1e-5, lam=0.5):
+    # TODO A C/C++ call would have a dramatically impact here (see https://realpython.com/python-bindings-overview/).
+    # marshalling data
+    L, U = [], []
+    locIdxB = {lstB[k]: k for k in range(len(lstB))}
+    for i in lstB:
+        iloc = locIdxB[i]
+        for j in G.neighbors(i):
+            if j < i and setB[j]:
+                jloc = locIdxB[j]
+                gij = G[i][j]
+                L.append([iloc, jloc, gij['l']])
+                U.append([iloc, jloc, gij['u']])
+    L, U = np.array(L), np.array(U)
+    # convert list of 3D points to a single array
+    y = x[lstB].reshape(3 * len(lstB))
+    if check:
+        checkDiff(funNLP, y, args=(tau, lam, L, U))
+    ans = minimize(funNLP, y, args=(tau, lam, L, U), method='BFGS', jac=True)
+    # retrieve solution
+    for k, i in enumerate(lstB):
+        x[i] = ans.x[(3 * k):(3 * (k + 1))]
