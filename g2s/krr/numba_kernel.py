0d34b1f894adcbe5fce634b74309a239e1c6b87e
Dom1L <lemm.d@web.de>
2021-06-02 15:34:46 +0200

    returned shapes are now consistent with the numpy kernel

--- g2s/krr/numba_kernel.py
+++ g2s/krr/numba_kernel.py
@@ -6,10 +6,10 @@ from numba import njit, prange
 def laplacian_kernel(feat1, feat2, sigma):
     a_size = feat1.shape[0]
     b_size = feat2.shape[0]
-    l1_norm = np.ones((a_size, b_size))
+    l1_norm = np.ones((b_size, a_size))
     for i in prange(a_size):
         for j in range(b_size):
-            l1_norm[i, j] = np.sum(np.abs(feat1[i] - feat2[j]))
+            l1_norm[j, i] = np.sum(np.abs(feat1[i] - feat2[j]))
     return np.exp(-1 * (l1_norm / sigma))
 
 
@@ -17,14 +17,16 @@ def laplacian_kernel(feat1, feat2, sigma):
 def gaussian_kernel(feat1, feat2, sigma):
     a_size = feat1.shape[0]
     b_size = feat2.shape[0]
-    l2_norm = np.ones((a_size, b_size))
+    l2_norm = np.ones((b_size, a_size))
     for i in prange(a_size):
         for j in range(b_size):
-            l2_norm[i, j] = np.sum(np.square(feat1[i] - feat2[j]))
+            l2_norm[j, i] = np.sum(np.square(feat1[i] - feat2[j]))
     return np.exp(-(l2_norm / (2*(sigma**2))))
 
 
 if __name__ == '__main__':
-    x_train = np.random.random((10000, 100))
-    gk = gaussian_kernel(x_train, x_train, 32)
+    x_train = np.random.random((1000, 100))
+    x_test = np.random.random((100, 100))
+
+    gk = gaussian_kernel(x_train, x_test, 32)
     lk = laplacian_kernel(x_train, x_train, 32)
